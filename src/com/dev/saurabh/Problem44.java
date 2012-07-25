b1ce906f062d56b9be3f90af27c8545745f69bb0
Saurabh Kumar Singh <saurabh.nsit@gmail.com>
2012-07-25 13:51:35 +0530

    SPOJ 1-Problem 44

--- src/com/dev/saurabh/Problem44.java
+++ src/com/dev/saurabh/Problem44.java
@@ -2,7 +2,6 @@ package com.dev.saurabh;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 
 /**
  * Copyright (c)
@@ -17,8 +16,31 @@ public class Problem44 {
     private static boolean firstOutput = true;
 
     private static boolean  [][] alphabetsAdjMatrix ;
+    private static int level = 0;
+    private static int[] V;
+    private static int[] inverseSubstitute;
+    private static int[] substitute;
     private static int alphabetAvailableCount;
+    private static int[] depth;
+    private static int[] result;
+    private static boolean[] isValidRank;
 
+    private static class Item implements Comparable<Item>
+    {
+        int count;
+        int index;
+
+        public Item(int cnt, int idx)
+        {
+            count = cnt;
+            index = idx;
+        }
+
+        @Override
+        public int compareTo(Item o) {
+            return count-o.count;  //To change body of implemented methods use File | Settings | File Templates.
+        }
+    }
     public static void main(String [] args)
     {
         try
@@ -42,11 +64,11 @@ public class Problem44 {
                 if(firstOutput)
                 {
                     System.out.println();
+                    firstOutput = false;
                 }
 
                 //resetVariables();
-
-                System.out.println(getDecyptedMessage(alphabetAvailableCount, lexicallyOrdered, encryptedMessage));
+                System.out.println(getDecyptedMessage(lexicallyOrdered, encryptedMessage));
 
             }
         }
@@ -56,17 +78,33 @@ public class Problem44 {
         }
     }
 
-    private static void resetVariables() {
+    private static String getDecyptedMessage(String[] lexicallyOrdered, String encryptedMessage)
+    {
+        StringBuilder out = new StringBuilder();
 
-        alphabetsAdjMatrix = new boolean[26][26];
-        alphabetAvailableCount = 0;
-    }
+        substitute =  new int[26];
+        inverseSubstitute = new int[26];
+        result = new int[26];
+        isValidRank = new boolean[26];
+        depth = new int[26];
+        alphabetsAdjMatrix  = new boolean[26][26];
 
-    private static String getDecyptedMessage(int alphabets, String[] lexicallyOrdered, String encryptedMessage) {
-        char [] encryptedKeys = new char[alphabets];
-        char [] decryptedKeys = new char[alphabets];
-        int [] alphabetRank = new int[alphabets];
-        alphabetsAdjMatrix  = new boolean[alphabets][alphabets];
+        for(int i=0; i<26; i++)
+        {
+            substitute[i] = i;
+            inverseSubstitute[i] = i;
+            result[i] = 0;
+            isValidRank[i] = false;
+            depth[i] = 0;
+            if (i >= alphabetAvailableCount && i < 25)
+            {
+                alphabetsAdjMatrix[i][i+1] = true;
+            }
+            else if (i < alphabetAvailableCount && alphabetAvailableCount < 26)
+            {
+                alphabetsAdjMatrix[i][alphabetAvailableCount] = true;
+            }
+        }
         //  alphabetsAdjMatrix[i][j]  = true will mean that (char)i+'alphabetAvailableCount' < (char)j+'alphabetAvailableCount'
         for(int i=0; i<lexicallyOrdered.length-1; i++)
         {
@@ -75,7 +113,6 @@ public class Problem44 {
             char firstChar = ' ';
             char secondChar = ' ';
             int count = 0;
-            boolean orderFound = false;
             while(count<first.length() && count<second.length())
             {
                 if(first.charAt(count) == second.charAt(count))
@@ -84,54 +121,94 @@ public class Problem44 {
                 }
                 else
                 {
-                    orderFound = true;
                     firstChar =  first.charAt(count);
                     secondChar = second.charAt(count);
-                    break;
-                }
-            }
-
-            if(orderFound)
-            {
-                alphabetsAdjMatrix[firstChar-'a'][secondChar-'a'] = true;
-                alphabetRank[firstChar-'a']++;
-                //look in to the row number secondChar-'a'. For all the column j which is true mark [firstChar-'a']
-                //as true
-                for(int j=0; j<alphabets; j++)
-                {
-                    if(alphabetsAdjMatrix[secondChar-'a'][j] == true)
+                    if(firstChar>='a' && secondChar>='a' && firstChar<='z' && secondChar<='z')
                     {
-                        if(! alphabetsAdjMatrix[firstChar-'a'][j])
-                        {
-                            alphabetsAdjMatrix[firstChar-'a'][j] = true;
-                            alphabetRank[firstChar-'a']++;
-                        }
-                    }
-                }
-                //look in to the same column as secondChar-'a' and mark all those places as true where [i][firstChar-'a']
-                //is true.
-                for(int j=0; j<alphabets; j++)
-                {
-                    if(alphabetsAdjMatrix[j][firstChar-'a'] == true)
-                    {
-                        if(! alphabetsAdjMatrix[j][secondChar-'a'])
-                        {
-                            alphabetsAdjMatrix[j][secondChar-'a'] = true;
-                            alphabetRank[j]++;
-                        }
+                        alphabetsAdjMatrix[firstChar-'a'][secondChar-'a'] = true;
+                        break;
                     }
                 }
             }
-
         }
 
-        int order = 0;
-        int count = 0;
-        for(int rank : alphabetRank)
+        V = new int[26];
+        for(int i=0; i<26;i++)V[i]=0;
+        level = 0;
+        for(int v=0; v< alphabetAvailableCount; v++)
         {
-            System.out.println("Rank of "+((char)(count++ +'a')) +" is: "+rank);
+            if (V[v] == 0)
+            {
+                dfsCreateSubstAndInvSubst(v);
+            }
         }
 
-        return ERROR_MESSAGE;
+        for(int v=0; v< alphabetAvailableCount; v++)
+        {
+            V = new int[26];
+            for(int i=0; i<26;i++)V[i]=0;
+            result[v] = alphabetAvailableCount - dfsCount(alphabetsAdjMatrix, inverseSubstitute[v]);
+        }
+
+        for(int v=0; v<26; v++)
+        {
+            if (v >= alphabetAvailableCount) {
+                isValidRank[v] = true;
+            } else {
+                isValidRank[v] = (result[v] == v && depth[inverseSubstitute[v]] == v);
+            }
+        }
+
+        for(int j=0; j<encryptedMessage.length(); j++)
+        {
+            char ch = encryptedMessage.charAt(j);
+            if(ch>='a' && ch<='z')
+            {
+                int enc = ch-'a';
+                int dec = substitute[enc];
+                if(isValidRank[dec])
+                {
+                    out.append((char)(dec+'a'));
+                }
+                else
+                {
+                    return ERROR_MESSAGE;
+                }
+            }
+            else
+            {
+                out.append(ch);
+            }
+        }
+
+        return out.toString();
+    }
+
+    private static void dfsCreateSubstAndInvSubst(int u) {
+        V[u] = 1;
+        for(int v =0; v<26; v++)
+        {
+            if (alphabetsAdjMatrix[u][v]) {
+                if (V[v] == 0) {
+                    dfsCreateSubstAndInvSubst(v);
+                }
+            }
+        }
+        inverseSubstitute[25-level] = u;
+        substitute[u] = 25 - level;
+        level++;
+    }
+
+    private static int dfsCount(boolean [][]adjMatrix, int u) {
+        V[u] = 1;
+        for(int v =0; v< alphabetAvailableCount; v++) {
+            if (adjMatrix[u][v]) {
+                if (V[v] == 0) {
+                    depth[v]++;
+                    V[u] += dfsCount(adjMatrix, v);
+                }
+            }
+        }
+        return V[u];
     }
 }
